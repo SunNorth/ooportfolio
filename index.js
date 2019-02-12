9cdd396d6cbca3304b1bb17d2006058d3d62c42d
Olesia Ol <thesunnorth@gmail.com>
2019-02-11 19:08:35 -0500

    added hamburger button setup

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-var lodash = require('lodash');
-exports.printMsg = function () {
-  console.log("This message is from index.js");
-};
+
+// Look for .hamburger
+var hamburger = document.querySelector(".hamburger");
+// On click
+hamburger.addEventListener("click", function () {
+  // Toggle class "is-active"
+  hamburger.classList.toggle("is-active");
+  // Do something else, like open/close menu
+});
