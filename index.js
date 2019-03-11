6ad0331b2b2f34a9cdbdfd3e9dd00bd3feeba535
Olesia Ol <thesunnorth@gmail.com>
2019-03-11 10:27:04 -0400

    before added bg

--- index.js
+++ index.js
@@ -6,3 +6,14 @@
 //   hamburger.classList.toggle("is-active");
 //   // Do something else, like open/close menu
 // });
+
+
+// change class .large to .small for div class=portfolio-img [4]
+
+$(window).on('resize', function () {
+  if ($(window).width() < 768) {
+    $('.portfolio-img').removeClass('large').addClass('small');
+  } else {
+    $('div.portfolio-img:nth-child(4)').removeClass('small').addClass('large');
+  }
+});
